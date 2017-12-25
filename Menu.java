46a5af8d9e0eee1c58620606964e7716e795d111
progettoingesoftware <34620499+progettoingesoftware@users.noreply.github.com>
2017-12-25 20:34:39 +0100

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -4,9 +4,12 @@ public class Menu
 {
     public static final String SALUTO_INIZIALE = "Benvenuto nell'applicazione per la gestione di risorse multimediali\n";
     public static final String SALUTO_FINALE = "Arrivederci, alla prossima!\n";
+    
+    public static final String ISCRIZIONE_AVVENUTA = "Complimenti, iscrizione avvenuta con successo!\n";
 
     public static final String SCELTA_MODALITA = "In quale modalità vuoi accedere?\n";
     public static final String SCELTA_AZIONE = "Cosa vuoi fare?\n";
+    public static final String INSERIMENTO_NOME = "Inserisci il tuo nome\n";
     	
     public static void mostraSalutoIniziale()
     {
@@ -18,6 +21,11 @@ public class Menu
 	     System.out.println(SALUTO_FINALE);
     }
     
+    public static void mostraMessaggioIscrizioneAvvenuta()
+    {
+	     System.out.println(ISCRIZIONE_AVVENUTA);
+    }
+    
     public static String mostraMenuSceltaModalita()
     {
     	 return (SCELTA_MODALITA + "1 - Fruitore\n" + "2 - Operatore\n" + "3 - Esci\n");
@@ -27,10 +35,12 @@ public class Menu
 	     System.out.println("3 - Esci"); */
     }
     
-    public static void eseguiMenuSceltaModalita()
+    public static int eseguiMenuSceltaModalita()
     {	 
     	 int comando = InputDati.leggiIntero(mostraMenuSceltaModalita(), 1, 3);
     	 
+    	 return comando;
+    	 /*
     	 switch(comando)
     	 {
     	 	case 1: mostraMenuSceltaAzione();
@@ -39,20 +49,35 @@ public class Menu
     	 	
     	 	case 3: mostraSalutoFinale();
     	 			break;   // termina l'esecuzione del programma
-    	 }
+    	 }*/
     	 
     }
     
-    public static String mostraMenuSceltaAzione()
+    public static void selettoreMenuSceltaModalita(int scelta)
+    {
+   	 		switch(scelta)
+   	 		{
+   	 			case 1: mostraMenuSceltaAzioneFruitore();
+   	 					break;
+   	 			case 2: //da definire (mostra menuSceltaAzioneOperatore
+   	 	
+   	 			case 3: mostraSalutoFinale();
+   	 					break;   // termina l'esecuzione del programma
+   	 		}
+    }
+    
+    public static String mostraMenuSceltaAzioneFruitore()
     {
     		return (SCELTA_AZIONE + "1 - Iscriviti come nuovo Fruitore\n" + "2 - Accedi se sei già iscritto\n" + "3 - Ritorna al menù precedente\n");
     }
     
-    public static void eseguiMenuSceltaAzione()
+    public static int eseguiMenuSceltaAzioneFruitore()
     {	 
-    	 int comando = InputDati.leggiIntero(mostraMenuSceltaAzione(), 1, 3);
+    	 int comando = InputDati.leggiIntero(mostraMenuSceltaAzioneFruitore(), 1, 3);
     	 
-    	 switch(comando)
+    	 return comando;
+    	 
+    	 /*switch(comando)
     	 {
     	 	case 1: mostraMenuSceltaAzione();///////////
     	 			break;
@@ -60,7 +85,32 @@ public class Menu
     	 	
     	 	case 3: mostraSalutoFinale();
     	 			break;   // termina l'esecuzione del programma
-    	 }
+    	 }*/
     	 
     }
+    
+    public static void selettoreMenuSceltaAzioneFruitore(int scelta)
+    {
+   	 		switch(scelta)
+   	 		{
+   	 			case 1: Fruitore nuovo = iscrizione();
+   	 					AnagraficaFruitori.aggiungiFruitore;
+   	 					mostraMessaggioIscrizioneAvvenuta();
+   	 					break;
+   	 			case 2: //da definire (mostra menuSceltaAzioneOperatore
+   	 	
+   	 			case 3: mostraSalutoFinale();
+   	 					break;   // termina l'esecuzione del programma
+   	 		}
+    }
+    
+    public static Fruitore iscrizione()
+    {
+    		String nome = InputDati.leggiStringaNonVuota(INSERIMENTO_NOME);
+    		
+    		
+    		
+    		return new Fruitore(nome,  )
+    }
+    
 }
--- Versione_1_Main.java
+++ Versione_1_Main.java
@@ -4,9 +4,17 @@ public class Versione_1_Main {
 
 	public static void main(String[] args) 
 	{
+		 AnagraficaFruitori Archivio = new AnagraficaFruitori();
+		 
 	     Menu.mostraSalutoIniziale();
 	     
-	     Menu.mostraMenuSceltaModalita(); //viene eseguito tutto nella classe Menu
+	     int scelta = Menu.eseguiMenuSceltaModalita(); //viene eseguito tutto nella classe Menu
+	     
+	     Menu.selettoreMenuSceltaModalita(scelta);
+	     
+	     scelta = Menu.eseguiMenuSceltaAzioneFruitore();
+	     
+	     Menu.selettoreMenuSceltaAzioneFruitore(scelta);
 	     
 	     
 	}
